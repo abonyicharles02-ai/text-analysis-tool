4c2dae2f19093821a57fda9237ac2c83343fb684
Charles Abonyi <abonyicharles02@gmail.com>
2026-09-23 15:28:03 +0100

    Completed file reading capability

--- analyze.py
+++ analyze.py
@@ -6,6 +6,7 @@ def welcomeUser():
 
 # Get Username
 def getUsername():
+    
     maxAttempts = 3
     attempts = 0
 
@@ -34,6 +35,16 @@ def getUsername():
 def greetUser(name):
     print("Hello, " + name)
 
+# Get text from file    
+def getArticleText():
+    f = open("files/article.txt", "r")
+    rawText = f.read()
+    f.close()
+    return rawText.replace("\n", "").replace("\r", "")
+
 welcomeUser()
 username = getUsername()
 greetUser(username)
+articleTextRaw = getArticleText()
+print("GOT:")
+print(articleTextRaw)
